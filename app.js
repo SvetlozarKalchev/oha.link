f1ab455ec805d7b928cc1432c25c5fd3a37afc99
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-08-14 16:33:10 +0300

    Add basic routing

--- app.js
+++ app.js
@@ -1,17 +1,74 @@
 var http = require('http');
 var url = require('url');
-var Shortener = require('./shortener.js');
+
+var ShortenerObject = require('./shortener.js');
+
+// Initiate DB connection
+var Shortener = new ShortenerObject();
+Shortener.db.connect_to_database();
 
 http.createServer(function(req, res) {
-  if(req.url != '/favicon.ico' && req.url == '/shorten') {
-    var q = url.parse(req.url, true);
-    console.log(q.query)
-    shortener.shorten(q.query['url'], function(err, result) {
-      res.write(result);
-      res.end();
+  /*
 
+    if !favicon && path.length == 5 {
+      find SURL and redirect
+    }
+    if !favicon && query == url {
+      shorten URL and return SURL
+    }
+    else {
+    return error
+    }
+
+  */
+  var parsedRequest = url.parse(req.url);
+
+  if(req.url == '/') {
+    res.write('Home');
+    res.end();
+  }
+  else if(req.url != '/favicon.ico' && parsedRequest.path.length == 4) {
+    Shortener.getShortenedLink('oha.link' + parsedRequest.path
+    , function (err, result) {
+      if(!err) {
+        res.writeHead(302,
+           {'Location': 'http://' + result});
+        res.end()
+      }
+      else {
+        console.log(err);
+        res.write("Invalid shortened URL.");
+        res.end();
+      }
     });
   }
+  else if(req.url != '/favicon.ico' && parsedRequest.pathname == '/shorten') {
+    Shortener.shorten(parsedRequest.query, function(err, result) {
+      // If a new URL gets shortened, an array with the length of 2
+      // gets returned
+      if(!err && result.length == 2) {
+        console.log('Shortened ' + result[0] + 'to: ' + result[1]);
+        res.write(result[1]);
+        res.end();
+      }
+      else if(!err && result.length > 2) {
+        console.log(result);
+        res.write(result);
+        res.end();
+      }
+      else {
+        console.log(err);
+        res.write('Error!');
+        res.end();
+      }
+    });
+  }
+  else {
+    res.write('The requested URL is invalid');
+    res.end();
+  }
+
+  /*
   else if(req.url != '/favicon.ico' && req.url.length <= 30){
     var q = url.parse(req.url, true);
 
@@ -32,9 +89,10 @@ http.createServer(function(req, res) {
       }
     })
   }
+
   else {
-    res.write('error');
+    res.write('Error 404');
     res.end();
-  }
+  }*/
 
 }).listen(8000);
