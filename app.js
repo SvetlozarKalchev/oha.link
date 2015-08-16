dc5fdacc8481b0cff52b451274256bfd8f8dadcc
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-08-16 18:15:35 +0300

    Add link to CSS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 var ShortenerObject = require('./shortener.js');
 
@@ -8,6 +9,8 @@ var Shortener = new ShortenerObject();
 Shortener.db.connect_to_database();
 
 http.createServer(function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Acces-Control-Allow-Methods", "GET");
   /*
 
     if !favicon && path.length == 5 {
@@ -24,10 +27,21 @@ http.createServer(function(req, res) {
   var parsedRequest = url.parse(req.url);
 
   if(req.url == '/') {
-    res.write('Home');
-    res.end();
+    fs.readFile('./ui/app.html', function(err, content) {
+      if(!err) {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.write(content);
+        res.end();
+      }
+      else {
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.write("Page not found.");
+        res.end();
+      }
+    });
   }
-  else if(req.url != '/favicon.ico' && parsedRequest.path.length == 4) {
+  else if(req.url != '/favicon.ico' && parsedRequest.path.length > 1
+   && !parsedRequest.query) {
     Shortener.getShortenedLink('oha.link' + parsedRequest.path
     , function (err, result) {
       if(!err) {
@@ -37,62 +51,42 @@ http.createServer(function(req, res) {
       }
       else {
         console.log(err);
-        res.write("Invalid shortened URL.");
+        res.write("Invalid short URL.");
         res.end();
       }
     });
   }
-  else if(req.url != '/favicon.ico' && parsedRequest.pathname == '/shorten') {
-    Shortener.shorten(parsedRequest.query, function(err, result) {
-      // If a new URL gets shortened, an array with the length of 2
-      // gets returned
-      if(!err && result.length == 2) {
-        console.log('Shortened ' + result[0] + 'to: ' + result[1]);
-        res.write(result[1]);
-        res.end();
-      }
-      else if(!err && result.length > 2) {
-        console.log(result);
-        res.write(result);
-        res.end();
-      }
-      else {
-        console.log(err);
-        res.write('Error!');
-        res.end();
-      }
-    });
+  else if(req.url != '/favicon.ico' && parsedRequest.pathname == '/shorten' && parsedRequest.query) {
+    if(parsedRequest.query.length >= 4) {
+      Shortener.shorten(parsedRequest.query, function(err, result) {
+        // If a new URL gets shortened, an array with the length of 2
+        // gets returned
+        if(!err && result.length == 2) {
+          console.log('Shortened ' + result[0] + 'to: ' + result[1]);
+          res.write(result[1]);
+          res.end();
+        }
+        else if(!err && result.length > 2) {
+          res.write(result)
+          res.end();
+        }
+        else {
+          console.log(err);
+          res.write('Error!');
+          res.end();
+        }
+      });
+    }
+    else {
+      console.log('ERR1:' + parsedRequest.query);
+      res.write('The requested URL is invalid');
+      res.end();
+    }
   }
   else {
-    res.write('The requested URL is invalid');
+    console.log('No such URL path');
+    res.write('The requested URL ' + parsedRequest.path +' is invalid');
     res.end();
   }
-
-  /*
-  else if(req.url != '/favicon.ico' && req.url.length <= 30){
-    var q = url.parse(req.url, true);
-
-    var link ='oha.link' + q['path'];
-
-    console.log('req url ' + link)
-
-    shortener.returnShortenedLink(link, function(err, result) {
-      if(!err && result !== null) {
-        console.log('301');
-        res.writeHead(301, {'Location': 'http://' + result[0]['URL']});
-
-        res.end();
-      }
-      else {
-        res.write('URL not found in DB');
-        res.end();
-      }
-    })
-  }
-
-  else {
-    res.write('Error 404');
-    res.end();
-  }*/
 
 }).listen(8000);
