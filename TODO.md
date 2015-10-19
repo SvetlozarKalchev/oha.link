4453df4a4d63bef0510f9ef5ea88b24d7dea9f28
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-19 22:32:35 +0200

    Refactor some more classes

--- TODO.md
+++ TODO.md
@@ -1,11 +1,4 @@
 #TODO:
-1. Create a Router class
-
-2. Add CONFIG file
-
-3. Rework DB class - fix DB object issues
-
-#TODO:
 
 1. Secure MongoDB port 28017 - it shouldn't be public!
 http://blog.mongodirector.com/10-tips-to-improve-your-mongodb-security/
@@ -24,3 +17,18 @@ http://blog.mongodirector.com/10-tips-to-improve-your-mongodb-security/
 6. Serve favicon.ico and robots.txt.
 
 7. Write a new front-end with an 'Copy to clipboard function'.
+
+# Routes & Algorithms
+
+- / -> Serve homepage static file
+
+- /[shortURL] -> -Look in DB
+								 	--If FOUND, return shortURL
+									--If NOT FOUND, return 404
+
+- /shorten?[url] -> -Look in DB for URL
+											--If FOUND, return shortURL
+											--If NOT FOUND, generateRandomString()
+												---Look in DB if isDuplicate()
+													----If FOUND, generateRandomString()
+													----If NOT FOUND, save url+shortURL and return shortURL
--- app.js
+++ app.js
@@ -1,87 +1,20 @@
+'use strict'
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var baseURL = require('./CONFIG.js').BASE_URL;
 
 // Initiate DB connection
-//var DataBase = require('./lib/database.js');
-var Shortener = require('./lib/shortenerController.js');
+let RequestController = require('./lib/RequestController.js');
 
 http.createServer(function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-nodemOrigin", "*");
   res.setHeader("Acces-Control-Allow-Methods", "GET");
 
-  var parsedRequest = url.parse(req.url);
+  let parsedRequest = url.parse(req.url);
 
-  if (req.url == '/') {
-    fs.readFile('./ui/app.html', function(err, content) {
-      if (!err) {
-        res.writeHead(200, {
-          'Content-type': 'text/html'
-        });
-        res.write(content);
-        res.end();
-      } else {
-        res.writeHead(404, {
-          'Content-type': 'text/html'
-        });
-        res.write("Page not found.");
-        res.end();
-      }
+  RequestController.route(parsedRequest, res,
+    function onResponseFinish(err, result) {
+      console.log(err, result)
     });
-  } else if (req.url != '/favicon.ico' && !parsedRequest.query) {
-    console.log('Searching for: ' + baseURL + parsedRequest.path);
-    Shortener.getShortenedLink(baseURL + parsedRequest.path
-      , function(err, result) {
-        if (!err) {
-          // If there is no protocol, add it, else redirect won't work.
-          if (result.toLowerCase().indexOf('http') !== -1) {
-            res.writeHead(302,
-              {
-                'Location': result
-              });
-            res.end()
-          } else {
-            res.writeHead(302,
-              {
-                'Location': 'http://' + result
-              });
-            res.end()
-          }
-        } else {
-          console.log(err);
-          res.write("Invalid short URL.");
-          res.end();
-        }
-      });
-  } else if (req.url != '/favicon.ico' && parsedRequest.pathname == '/shorten' && parsedRequest.query) {
-    if (parsedRequest.query.length >= 4) {
-      console.log("PRQ: " + parsedRequest.query);
-      Shortener.shorten(parsedRequest.query, function(err, result) {
-        // If a new URL gets shortened, an array with the length of 2
-        // gets returned
-        if (!err && result.length == 2) {
-          console.log('Shortened ' + result[0] + ' to: ' + result[1]);
-          res.write(result[1]);
-          res.end();
-        } else if (!err && result.length > 2) {
-          res.write(result)
-          res.end();
-        } else {
-          console.log(err);
-          res.write('Error!');
-          res.end();
-        }
-      });
-    } else {
-      console.log('ERR1:' + parsedRequest.query);
-      res.write('The requested URL is invalid');
-      res.end();
-    }
-  } else {
-    console.log('No such URL path');
-    res.write('The requested URL ' + parsedRequest + ' is invalid');
-    res.end();
-  }
-
 }).listen(8000);
