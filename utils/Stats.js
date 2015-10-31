dc86f753d2ec6741e84593fcdeb8d57269c72988
SvetlozarKalchev <svetlozar@kalchev.eu>
2015-10-31 20:13:50 +0100

    Add basic logging

--- utils/Stats.js
+++ utils/Stats.js
@@ -2,20 +2,43 @@
 let Scribo = require('scribo');
 
 module.exports = (function() {
+  let counter = 0;
 
-  function logReqToRoot(httpReq) {
+  function getIp(httpReq) {
     let reqIp = httpReq.headers['x-forwarded-for'] || httpReq.connection.remoteAddress;
 
+    return reqIp;
+  }
+
+  function getLogMessage(httpReq) {
     let logMessage = ` from
-    ${reqIp}
+    ${getIp(httpReq)}
     ${httpReq.headers['user-agent']} |
     ${httpReq.headers['referer']}
     ----------------------------------------------------------- '\n'`;
 
-    Scribo.logEvent('Request to /', logMessage, true);
+    return logMessage;
+  }
+
+  function logReqToRoot(httpReq) {
+    Scribo.logEvent('Request to /', getLogMessage(httpReq), true);
+  }
+
+  // function logReqForShortLink(httpReq, parsedReq) {
+  //   let shortLink = `${parsedReq.pathName}`;
+  //
+  //   Scribo.logEvent('Request to /', logMessage, true);
+  // }
+
+  function incrementVisits(httpReq) {
+    counter++;
+    let logMessage = `from ${getIp(httpReq)}, total visits: ${counter}`;
+
+    Scribo.logEvent('visit', logMessage, true);
   }
 
   return {
-    logReqToRoot: logReqToRoot
+    logReqToRoot: logReqToRoot,
+    incrementVisits: incrementVisits
   }
 })();
